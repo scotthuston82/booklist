c1b2e8ba4bffe7a784a68e0aa7a8e52871eea7bf
Scott <scotthuston1982@gmail.com>
2018-02-05 09:37:24 -0500

    finished

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,28 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv do
+        Book.generate_csv(@books)
+      end
+      format.json do
+        render json: @books
+      end
+    end
+  end
 
 end
+
+# def index
+#   @people = Person.all
+#
+#   respond_to do |format|
+#     format.html
+#     format.js
+#     format.xml { render xml: @people }
+#   end
+# end
